5df26e61df89f36bbb66f3f93d4fee3ac0ea67a8
Unknown <382167003@qq.com>
2019-05-15 20:18:42 +0800

    将产品列表products_list添加进去

--- blog/urls.py
+++ blog/urls.py
@@ -16,9 +16,9 @@ Including another URLconf
 from django.urls import path
 from . import views
 
-urlpatterns = [
-    path('', views.blog_page),
-    path('<int:blog_id>', views.blog_text),
 
+urlpatterns = [
+    path('', views.blog_page,),
+    path('<int:blog_id>', views.blog_text),
 
 ] 
